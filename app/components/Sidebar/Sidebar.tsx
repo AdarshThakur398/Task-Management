bce9f47e0ed1e546befb3bccf04be7945397078a
AdarshThakur398 <nutius90@gmail.com>
2025-02-01 17:23:05 +0530

    sidebar completed

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -133,39 +133,42 @@ color: ${(props) => props.theme.colorGrey3};
 
 .nav-item {
   position: relative;
-  padding: 0.6rem 1rem 1rem 2.1rem;
+  padding: 0.8rem 1rem 0.9rem 2.1rem;
   margin: 0.3rem 0;
   display: grid;
   grid-template-columns: 40px 1fr;
   cursor: pointer;
+  align-items:center;
 
-  &::after {
-    position: absolute;
-    content: "";
-    left: 0;
-    top: 0;
-    width: 0;
-    height: 100%;
-    background-color: ${(props) => props.theme.activeNavLinkHover};
-    z-index: 1;
-    transition: all 0.3s ease-in-out;
-  }
-    &::after {
-     position: absolute;
-    content: "";
-    left: 0;
-    top: 0;
-    width: 0;
-    height: 100%;
-    background-color: ${(props) => props.theme.colorGreenDark};
-    border-top-left-radius:5px;
-    border-bottom-left-radius:5px;
-     }
+   &::after {
+      position: absolute;
+      content: "";
+      left: 0;
+      top: 0;
+      width: 0;
+      height: 100%;
+      background-color: ${(props) => props.theme.activeNavLinkHover};
+      z-index: 1;
+      transition: all 0.3s ease-in-out;
+    }
+ &::before {
+      position: absolute;
+      content: "";
+      right: 0;
+      top: 0;
+      width: 0%;
+      height: 100%;
+      background-color: ${(props) => props.theme.colorGreenDark};
+
+      border-bottom-left-radius: 5px;
+      border-top-left-radius: 5px;
+    } 
 
     a {
     font-weight:500;
     transition:all 0.3s ease-in-out;
-    z-index:2;}
+    z-index:2;
+    line-height:0;}
     i {
     display:flex;
     align-items:center;
@@ -176,6 +179,15 @@ color: ${(props) => props.theme.colorGrey3};
     width:100%;
     }}
 }
+    .active {
+    background-color:${(props) => props.theme.activeNavLink};
+    i,a{
+    color:${(props) => props.theme.colorIcons2}};
+    line-height:0;
+    
+    }
+    .active::before {
+    width:0.3rem;}
 `;
 
 
