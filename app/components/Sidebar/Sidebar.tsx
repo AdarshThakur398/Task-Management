fcef6915c5ab448826c9c6438abe258dd57b6ddd
AdarshThakur398 <nutius90@gmail.com>
2025-02-01 15:43:43 +0530

    profile icon in sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,27 +1,117 @@
-"use client"
-import React from 'react'
-import styled from 'styled-components'
-import {useGlobalState} from '@/app/context/GlobalProvider';
-
-
+"use client";
+import React from "react";
+import styled from "styled-components";
+import { useGlobalState } from "@/app/context/GlobalProvider";
+import Image from "next/image";
+import menu from "@/app/utils/menu"
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 const Sidebar = () => {
-    const {theme} = useGlobalState();
-  
+  const { theme } = useGlobalState();
+  const router=useRouter();
+  const pathname=usePathname();
+  const handleClick= (link:string) => {
+    router.push(link);
+  }
   return (
     <div>
-      
-     <SidebarStyles theme={theme}><div className="profile"><div className="profile-overlay">
-        <Image width={70} height={70} src="to-do-list-task-management-or-completion-tracking-or-reminder-to-finish-assignment-work-planning-or-schedule-concept-productive-businessman-with-pencil-and-to-do-list-clipboard-with-alarm-clock-vector.jpg" alt="profile"/></div></div></SidebarStyles> 
+      <SidebarStyles theme={theme}>
+        <div className="profile">
+          <div className="profile-overlay"></div>
+           
+             <div className="image">
+              <Image
+                width={100}
+                height={100}
+                src="/to-do-list-task-management-or-completion-tracking-or-reminder-to-finish-assignment-work-planning-or-schedule-concept-productive-businessman-with-pencil-and-to-do-list-clipboard-with-alarm-clock-vector.jpg"
+                alt="profile"
+              />
+             </div><h3>
+              <span>adarsh</span>
+              <span>hero</span>
+            </h3></div>
+            
+         
+          <ul className="nav-items">{menu.map((item)=> {
+            const link=item.link;
+return <li  key={item.link} className={`nav-item ${pathname===link ? "active" : ""}`} onClick={() => {
+  handleClick(link);
+}}>
+      {item.icon}
+      <Link href={item.link}>
+      {item.title}</Link>
+</li>
+          })}</ul>
+          <button></button>
+       
+      </SidebarStyles>
     </div>
-  )
-}
-const SidebarStyles=styled.nav`
-  position:relative;
+  );
+};
+const SidebarStyles = styled.nav`
+  position: relative;
   width: ${(props) => props.theme.sidebarWidth};
-  
-  background-color:${(props) => props.theme.colorBg2};
-  border: 2px solid ${(props) => props.theme.borderColor2};
-  border-radius:1rem;
-  `;
+  height:100%;
 
-export default Sidebar
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+  color: ${(props) => props.theme.colorGrey3};
+  .profile {
+  margin:1.5rem;
+  padding:1rem 0.8rem;
+  position:relative;
+  border-radius:1rem;
+  cursor:pointer;
+  font-weight:500;
+  color: ${(props) => props.theme.colorGrey0};
+  display:flex;
+  align-items:center;
+  .profile-overlay {
+  position:absolute;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  backdrop-filter:blur(10px);
+  z-index:0;
+  background:${(props) => props.theme.colorBg3};
+  transition:all 0.55s linear;
+  border-radius:1rem;
+  border:2px solid ${(props)=> props.theme.borderColor2};
+  opacity:0.2;
+
+}
+  h3 {
+  font-size:1.2rem;
+  display:flex;
+  flex-direction:column;
+  line-height:1.4rem;}
+  
+  .image,h3 {
+  position:relative;
+  z-index:1;}
+  
+  .image {
+  flex-shrink:0;
+  display:inline-block;
+  overflow:hidden;
+  transition: all 0.5s ease;
+  border-radius: 100%;
+  width:70px;
+  height:70px;
+  img {
+  height:100%;
+  width:auto;
+  transition: all 0.5s ease;}}
+  }
+  image,h3 {
+   margin-left:0.9rem;
+   font-size:clamp(1.2rem,4vw,1.4rem);
+   line-height:100%;}
+`;
+
+export default Sidebar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import GlobalStyleProvider from "./providers/GlobalStyleProvider";
+import ContextProvider from "./providers/ContextProvider";
 
 
 const geistSans = Geist({
@@ -27,11 +28,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ><GlobalStyleProvider>
+      >
+        <ContextProvider><GlobalStyleProvider>
         <Sidebar/>
-        {children}</GlobalStyleProvider>
+        {children}</GlobalStyleProvider></ContextProvider>
       </body>
     </html>
   );
